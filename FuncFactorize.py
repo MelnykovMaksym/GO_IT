e39bba17bec1b87b004971194198eac1c0663e68
MelnykovMaksym <sunbeam407@gmail.com>
2021-10-10 21:36:00 +0200

    Factorize

--- FuncFactorize.py
+++ FuncFactorize.py
@@ -14,7 +14,7 @@ def factorize(number):
                 continue
         else:
             break
-    print(lst)
+    return lst
 
 
 if __name__ == '__main__':
@@ -22,14 +22,13 @@ if __name__ == '__main__':
 
     start = time.perf_counter()
     with Pool() as p:
-        p.map(factorize, (a, b, c, d))
+        a, b, c, d = p.map(factorize, (a, b, c, d))
     finish = time.perf_counter()
 
     print(f'Task done in {round(finish - start, 3)} sec')
-    # print(a, b, c, d)
 
-    # assert a == [1, 2, 4, 8, 16, 32, 64, 128]
-    # assert b == [1, 3, 5, 15, 17, 51, 85, 255]
-    # assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
-    # assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106,
-    #              1521580, 2130212, 2662765, 5325530, 10651060]
+    assert a == [1, 2, 4, 8, 16, 32, 64, 128]
+    assert b == [1, 3, 5, 15, 17, 51, 85, 255]
+    assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+    assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106,
+                 1521580, 2130212, 2662765, 5325530, 10651060]
