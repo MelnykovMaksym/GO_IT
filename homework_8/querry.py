37fcf9055692cfaa4f9e6a6eb0740149fccee02d
MelnykovMaksym <sunbeam407@gmail.com>
2021-11-16 21:21:13 +0100

    Add folder Screens

--- homework_8/querry.py
+++ homework_8/querry.py
@@ -1,4 +1,4 @@
-# 5 студентов с наибольшим средним баллом по всем предметам.
+#1) 5 студентов с наибольшим средним баллом по всем предметам.
     select last_name, avg_scores
     from students
     join (select avg(score) as avg_scores,
@@ -6,7 +6,7 @@
     group by student_id) as avm
     on students.id = avm.student_id
     order by avg_scores desc fetch first 5 row only
-# 1 студент с наивысшим средним баллом по одному предмету.
+#2) 1 студент с наивысшим средним баллом по одному предмету.
     select group_name, subject, last_name, avg(score) from scores
     join students
     on students.id = scores.student_id
@@ -15,7 +15,7 @@
     where subject = 'chemistry'
     group by groups.group_name, scores.subject, students.last_name, scores.score
     order by score desc limit 1
-# средний балл в группе по одному предмету.
+#3) средний балл в группе по одному предмету.
     select group_name, subject, avg(score) from scores
     join students
     on students.id = scores.student_id
@@ -23,23 +23,23 @@
     on groups.id = students.group_id
     where group_name = 'economic_121' and subject = 'chemistry'
     group by groups.group_name, scores.subject
-# Средний балл в потоке.
+#4) Средний балл в потоке.
     select avg(score) from scores
-# Какие курсы читает преподаватель.
+#5) Какие курсы читает преподаватель.
     select distinct tutor, subject from scores
     where tutor = 'Popov'
-# Список студентов в группе.
+#6) Список студентов в группе.
     select group_name, first_name
     from groups, students
     where group_name = 'economic_121'
-# Оценки студентов в группе по предмету.
+#7) Оценки студентов в группе по предмету.
     select score, group_name from scores
     join students
     on students.id = scores.student_id
     join groups
     on groups.id = students.group_id
     where group_name = 'economic_121'
-# Оценки студентов в группе по предмету на последнем занятии.
+#8) Оценки студентов в группе по предмету на последнем занятии.
     select group_name, score, date from scores
     join students
     on students.id = scores.student_id
@@ -47,21 +47,21 @@
     on groups.id = students.group_id
     where group_name = 'economic_121'
     order by date desc limit 1
-# Список курсов, которые посещает студент.
+#9) Список курсов, которые посещает студент.
     select DISTINCT subject, last_name from scores
     join students
     on students.id = scores.student_id
     where last_name = 'Gyorgy'
-# Список курсов, которые студенту читает преподаватель.
+#10) Список курсов, которые студенту читает преподаватель.
     select DISTINCT tutor, last_name, subject from scores
     join students
     on students.id = scores.student_id
     where last_name = 'Dare' and tutor = 'Popov'
-# Средний балл, который преподаватель ставит студенту.
+#11) Средний балл, который преподаватель ставит студенту.
     select tutor, last_name, avg(score) from scores
     join students
     on students.id = scores.student_id
     group by tutor, students.last_name order by tutor
-# Средний балл, который ставит преподаватель.
+#12) Средний балл, который ставит преподаватель.
     select tutor, avg(score) from scores
     group by tutor order by avg(score)
